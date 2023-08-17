6fca331cac8ce69fea1242a928fd9c1a3343af8a
Irfan Hanjra <ihanjra@gmail.com>
2023-08-17 13:25:06 -0400

    Completed nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <div className="nav">
+      <ul>
+        <li className="nav-home">
+          <Link to="/">Humpty Dumb Tees</Link>
+        </li>
+        <li className="nav-shirts">
+          <Link to="/shirts">Shirts</Link>
+        </li>
+        <li className="nav-createshirts">
+          <Link to="/shirts/create-shirt">New Shirt</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Nav
